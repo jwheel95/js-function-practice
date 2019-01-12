f084bff59ea377dc012d50759710c77ed3a78664
Joe Wheelock <joemwheelock@gmail.com>
2019-01-11 18:32:45 -0800

    first arr function

--- js/scripts.js
+++ js/scripts.js
@@ -1,31 +1,35 @@
 // Basic function review
 
-function isEven(num) {
-  return num % 2 ===0;
-}
+// function isEven(num) {
+//   return num % 2 ===0;
+// }
 
-function factorial(num) {
-  let result = 1;
-  for(i = 1; i <= num; i++) {
-    result *= i;
-  }
-  return result;
-}
+// function factorial(num) {
+//   let result = 1;
+//   for(i = 1; i <= num; i++) {
+//     result *= i;
+//   }
+//   return result;
+// }
 
-function kebabToSnake(str) {
-  const snake = str.replace(/-/g, "_");
-  return snake;
-}
+// function kebabToSnake(str) {
+//   const snake = str.replace(/-/g, "_");
+//   return snake;
+// }
 
 // Higher Order Functions
 // (passing a function into another function)
 
-function sing() {
-  console.log("twinkle twinkle");
-  console.log("how I wonder...");
-}
+// function sing() {
+//   console.log("twinkle twinkle");
+//   console.log("how I wonder...");
+// }
 
-setInterval(sing, 5000);
-
+// setInterval(sing, 5000);
 
 // Practice with Arrays
+function printReverse(arr) {
+  for(i = arr.length -1; i >= 0; i--) {
+    console.log(arr[i]);
+  };
+}
