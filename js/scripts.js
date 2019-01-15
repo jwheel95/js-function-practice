53a51644c08ed148d60ffe474112b214d192cb8a
Joe Wheelock <joemwheelock@gmail.com>
2019-01-14 17:11:19 -0800

    completed movie db challenge

--- js/scripts.js
+++ js/scripts.js
@@ -28,44 +28,79 @@
 // setInterval(sing, 5000);
 
 // Practice with Arrays
-function printReverse(arr) {
-  for(i = arr.length -1; i >= 0; i--) {
-    console.log(arr[i]);
+// function printReverse(arr) {
+//   for(i = arr.length -1; i >= 0; i--) {
+//     console.log(arr[i]);
+//   }
+// }
+
+// function isUniform(arr) {
+//   let first = arr[0];
+//   for(i = 0; i < arr.length; i++) {
+//     if(arr[i] !== first) {
+//       return false;
+//     }
+//   }
+//   return true;
+// }
+
+// function sumArray(arr) {
+//   let answer = 0;
+//   arr.forEach(function(i) {
+//     answer += i;
+//   })
+//   return answer;
+// }
+
+// function max(arr) {
+//   let maxNum = arr[0];
+//   arr.forEach(function(i) {
+//     if (i > maxNum) {
+//       maxNum = i;
+//     }
+//   })
+//   return maxNum;
+// }
+
+// // BUILD MY OWN FOREACH
+
+// function myForEach(arr,func) {
+//   for(i = 0; i < arr.length; i++) {
+//     func(arr[i]);
+//   }
+// }
+
+// JS OBJECT DATABASE ARRAY
+const movieDb = [
+  {
+    name: "Pan's Labyrinth",
+    rating: "5 stars",
+    haveSeen: true
+  },
+  {
+    name: "Devil's Backbone",
+    rating: "4 stars",
+    haveSeen: true
+  },
+  {
+    name: "Pacific Rim",
+    rating: "3 stars",
+    haveSeen: false
+  },
+  {
+    name: "Hellboy",
+    rating: "3.5 stars",
+    haveSeen: false
   }
-}
+]
 
-function isUniform(arr) {
-  let first = arr[0];
-  for(i = 0; i < arr.length; i++) {
-    if(arr[i] !== first) {
-      return false;
-    }
+movieDb.forEach(function(movie) {
+  let result = "You have ";
+  if(movie.haveSeen === true) {
+    result += "seen " + movie.name + ".";
+  } else {
+    result += "not watched " + movie.name + ".";
   }
-  return true;
-}
-
-function sumArray(arr) {
-  let answer = 0;
-  arr.forEach(function(i) {
-    answer += i;
-  })
-  return answer;
-}
-
-function max(arr) {
-  let maxNum = arr[0];
-  arr.forEach(function(i) {
-    if (i > maxNum) {
-      maxNum = i;
-    }
-  })
-  return maxNum;
-}
-
-// BUILD MY OWN FOREACH
-
-function myForEach(arr,func) {
-  for(i = 0; i < arr.length; i++) {
-    func(arr[i]);
-  }
-}
+  result += " It has " + movie.rating + " out of 5.";
+  console.log(result);
+})
