e1c7cd58467a73d4713c74b623b1e806db8dc729
Joe Wheelock <joemwheelock@gmail.com>
2019-01-22 02:47:16 -0800

    completed color-toggle func

--- js/scripts.js
+++ js/scripts.js
@@ -71,36 +71,48 @@
 // }
 
 // JS OBJECT DATABASE ARRAY
-const movieDb = [
-  {
-    name: "Pan's Labyrinth",
-    rating: "5 stars",
-    haveSeen: true
-  },
-  {
-    name: "Devil's Backbone",
-    rating: "4 stars",
-    haveSeen: true
-  },
-  {
-    name: "Pacific Rim",
-    rating: "3 stars",
-    haveSeen: false
-  },
-  {
-    name: "Hellboy",
-    rating: "3.5 stars",
-    haveSeen: false
-  }
-]
+// const movieDb = [
+//   {
+//     name: "Pan's Labyrinth",
+//     rating: "5 stars",
+//     haveSeen: true
+//   },
+//   {
+//     name: "Devil's Backbone",
+//     rating: "4 stars",
+//     haveSeen: true
+//   },
+//   {
+//     name: "Pacific Rim",
+//     rating: "3 stars",
+//     haveSeen: false
+//   },
+//   {
+//     name: "Hellboy",
+//     rating: "3.5 stars",
+//     haveSeen: false
+//   }
+// ]
 
-movieDb.forEach(function(movie) {
-  let result = "You have ";
-  if(movie.haveSeen === true) {
-    result += "seen " + movie.name + ".";
+// movieDb.forEach(function(movie) {
+//   let result = "You have ";
+//   if(movie.haveSeen === true) {
+//     result += "seen " + movie.name + ".";
+//   } else {
+//     result += "not watched " + movie.name + ".";
+//   }
+//   result += " It has " + movie.rating + " out of 5.";
+//   console.log(result);
+// })
+
+const myBtn = document.querySelector('#btn');
+let isPurple = false;
+
+myBtn.addEventListener('click', function() {
+  if (isPurple) {
+    document.body.style.background = "white";
   } else {
-    result += "not watched " + movie.name + ".";
-  }
-  result += " It has " + movie.rating + " out of 5.";
-  console.log(result);
-})
+    document.body.style.background = "purple";
+  };
+  isPurple = !isPurple;
+});
